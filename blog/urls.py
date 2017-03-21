89ff16ddb37b2795278e1134ac334ebe82c6939c
Andrey <nomail@bk.ru>
2017-03-21 18:03:35 +0800

    And it works now!

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name = 'post_list')
+    url(r'^$', views.post_list, name = 'post_list'),
+    url(r'^sample.html', views.where_is_my_sample, name = 'sample')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def post_list(request):
     return render(request,
                   'blog/post_list.html', {})
+
+def where_is_my_sample(request):
+    return render(request, 'blog/sample.html', {})
