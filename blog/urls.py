a896e3e78971414976b6dcc6509145374b8476b6
Andrey <nomail@bk.ru>
2017-03-22 15:31:44 +0800

    'Расширение+'

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name = 'post_list'),
-    url(r'^sample.html', views.where_is_my_sample, name = 'sample')
+    url(r'^sample.html', views.where_is_my_sample, name = 'sample'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
