d0e4a07867935e7c4744d88311e1845fc52d7cc6
Andrey <nomail@bk.ru>
2017-03-22 18:20:58 +0800

    Secutrity & Forms

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name = 'post_list'),
-    url(r'^sample.html', views.where_is_my_sample, name = 'sample'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^sample.html', views.where_is_my_sample, name='sample'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
